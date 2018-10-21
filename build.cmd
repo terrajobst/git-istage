86ec94e5c706cac00e24aaf296a441f74ca22d88
Immo Landwerth <immo@landwerth.net>
2018-10-20 18:17:41 -0700

    Simplify build

--- build.cmd
+++ build.cmd
@@ -1,13 +1,3 @@
 @echo off
-setlocal
 
-:: Find MSBuild to use
-
-for /f "tokens=*" %%i in ('"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -property installationPath') do set VSPATH=%%i
-set MSBUILD_PATH=%VSPATH%\MSBuild\15.0\Bin\MSBuild.exe
-set BIN_PATH=%~dp0bin
-
-:: Run build
-
-if not exist %BIN_PATH% mkdir %BIN_PATH%
-"%MSBUILD_PATH%" /nologo /m /v:m /nr:false /bl:%BIN_PATH%\msbuild.binlog %*
+dotnet build %~dp0\src\git-istage.sln /nologo
