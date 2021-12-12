64a4c63489a868224c9f9dd1a8859e3cfaac52b7
Juan Manuel Elosegui <elosegui@gmail.com>
2021-12-12 18:06:11 -0500

    Add unit test to verify the content of the temp patch file.

---
+++ src/git-istage.tests/PatchingTests.cs
@@ -0,0 +1,35 @@
+﻿using Moq;
+using Xunit;
+using System.Linq;
+using System.IO;
+
+namespace GitIStage.Tests
+{
+    public class PatchingTests
+    {
+        [Fact]
+        public void Patching_PatchContentShouldNotChange()
+        {
+            var processRunner = new Mock<IProcessRunner>();
+            string actualPatchContent = null;
+            processRunner.Setup(p => p.Run(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<bool>()))
+                         .Callback<string, string, string, bool>((g, a, w, r) =>
+                         {
+                             var patchFilePath = a.Split(' ').LastOrDefault()?.Trim('"');
+                             if (File.Exists(patchFilePath))
+                             {
+                                 actualPatchContent = File.ReadAllText(patchFilePath);
+                             }
+                         })
+                         .Returns(Enumerable.Empty<string>());
+
+            var patching = new Patching(processRunner.Object);
+
+            var expectedPatchContent = $@"line1{'\n'}line2";
+
+            patching.ApplyPatch("gitpath", "workingDirectory", expectedPatchContent, PatchDirection.Stage);
+
+            Assert.Equal(expectedPatchContent, actualPatchContent);
+        }
+    }
+}
