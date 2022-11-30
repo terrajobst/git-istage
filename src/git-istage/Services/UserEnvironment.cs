7e6f1c3cccff7098d03d68ffab89313840e0451b
Immo Landwerth <immo@landwerth.net>
2022-11-30 11:49:50 -0800

    Fix whitespace

--- src/git-istage/Services/UserEnvironment.cs
+++ src/git-istage/Services/UserEnvironment.cs
@@ -8,17 +8,17 @@ internal sealed class UserEnvironment
         {
             var configDirectory = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
             settingsDirectory = Path.Join(configDirectory, ".git-istage");
-            
+
             // Migrate previous location
             var userProfile = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
             var oldSettingsDirectory = Path.Join(userProfile, ".git-istage");
-        
+
             if (Directory.Exists(oldSettingsDirectory) && !Directory.Exists(settingsDirectory))
             {
                 var parentDirectory = Path.GetDirectoryName(settingsDirectory);
                 if (parentDirectory is not null)
                     Directory.CreateDirectory(parentDirectory);
-                
+
                 Directory.Move(oldSettingsDirectory, settingsDirectory);
 
                 Console.WriteLine($"info: settings were migrated from '{oldSettingsDirectory}' to '{settingsDirectory}'");
